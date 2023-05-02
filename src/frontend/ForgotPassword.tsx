ce56c5d95e260f547698239b7c449e6b8099f338
TechRufy <alb.genovese@gmail.com>
2023-05-02 12:49:58 +0200

    sezione password dimenticata

--- src/frontend/ForgotPassword.tsx
+++ src/frontend/ForgotPassword.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import {ScrollView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Pressable, ScrollView, Text, TextInput, View} from 'react-native';
 import Style from './Stili';
 
 import {StackNavigationProp} from '@react-navigation/stack';
 import {AppStackParamList} from './App';
+import ImageBox from './props/ImageBox';
+import {logo} from '../images';
 type ProfileScreenNavigationProp = StackNavigationProp<
   AppStackParamList,
   'ForgotPassword'
@@ -14,11 +16,62 @@ type Props = {
 };
 
 const ForgotPassword = ({navigation}: Props) => {
+  const [email, setemail] = useState('');
+  const [error, setError] = useState('');
+
   return (
     <ScrollView style={Style.containerPagina}>
-      <Text>Ciao</Text>
+      <ImageBox image={logo} direction="horizontal"></ImageBox>
+      <Text style={[Style.titolo, {fontSize: 17, paddingHorizontal: 20}]}>
+        Enter your Spotify username. We will send an email to reset your
+        password{' '}
+      </Text>
+      <View style={{paddingTop: '3%'}}>
+        <TextInput
+          style={Style.Input}
+          placeholder="Username"
+          onChangeText={setemail}></TextInput>
+      </View>
+      <Pressable
+        style={Style.contenitoreLogin}
+        onPress={async () => {
+          setError('');
+          var risposta = await ResetPassword(email);
+          if (risposta.esito.code != undefined) {
+            setError('email non esistente');
+          } else {
+            setError(risposta.esito);
+          }
+        }}>
+        <Text style={Style.testo}>Reset password</Text>
+      </Pressable>
+      <Text style={Style.error}>{error}</Text>
     </ScrollView>
   );
 };
+
+async function ResetPassword(email: string) {
+  var risposta = await fetch(
+    'https://backendfakespotify.onrender.com/ResetPassword',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: email,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    },
+  )
+    .then(Response => {
+      return Response.json();
+    })
+    .then(json => {
+      return json;
+    });
+  console.log(risposta);
+  return risposta;
+}
 
 export default ForgotPassword;
